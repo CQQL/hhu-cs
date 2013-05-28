ab7fafbee83134d3b51326a0a0219f40948cdba6
Marten Lienen <marten.lienen@gmail.com>
2013-05-28 21:14:11 +0200

    Add comments

--- Info2/exercises/6.1.asm
+++ Info2/exercises/6.1.asm
@@ -13,21 +13,31 @@ asm_main:
   enter 0, 0
   pusha
 
+  ; Lege eine lokale Variable mit Wert 0 an
   push DWORD 0
+
+  ; Lese eine Zahl in die lokale Variable
   push esp
   push input_format
   call scanf
   add esp, 8
 
+  ; Isoliere die ersten 29 Bit (TAG) und lege sie auf den Stack als Argument für printf
   mov eax, [esp]
   shr eax, 3
   push eax
+
+  ; Isoliere Bit 2 und 3 (LINE) und lege sie auf den Stack als Argument für printf
   mov eax, [esp + 4]
   and eax, 110b
   shr eax, 1
   push eax
+
+  ; Ergebnisse ausgeben
   push output_format
   call printf
+
+  ; Stack zurücksetzen
   add esp, 16
   
   popa
--- Info2/exercises/6.2.asm
+++ Info2/exercises/6.2.asm
@@ -6,22 +6,26 @@ segment .text
   global asm_main
   extern scanf
   extern printf
+
 asm_main:
   enter 0, 0
   pusha
   mov ebp, esp
 
+  ; Lokale Variable anlegen
   push DWORD 0
 
-  ; Load number into esp
+  ; n in die lokale Variable einlesen
   push esp
   push input_format
   call scanf
   add esp, 8
 
+  ; Die lokale Variable liegt jetzt oben auf dem Stack und kann so an fib übergeben werden
   call fib
   add esp, 4
 
+  ; fib(n) ausgeben
   push eax
   push output_format
   call printf
@@ -32,18 +36,20 @@ asm_main:
   leave
   ret
 
-; Compute the n-th fibonacci number
+; Berechnung von fib(n)
 fib:
   enter 0, 0
   push ebx
 
+  ; fib(0) = 0
   cmp [ebp + 8], DWORD 0
   je n_is_zero
 
+  ; fib(1) = 1
   cmp [ebp + 8], DWORD 1
   je n_is_one
 
-  ; Compute fib(n - 1) and save it in ebp
+  ; fib(n - 1) berechnen und in ebx speichern
   mov eax, [ebp + 8]
   sub eax, 1
   push eax
@@ -51,26 +57,29 @@ fib:
   add esp, 4
   mov ebx, eax
 
-  ; Compute fib(n - 2)
+  ; fib(n - 2) berechnen
   mov eax, [ebp + 8]
   sub eax, 2
   push eax
   call fib
   add esp, 4
   
-  ; Add the results 
+  ; fib(n) = fib(n - 1) + fib(n - 2)
   add eax, ebx
 
   jmp return
 
+; 0 zurückgeben
 n_is_zero:
   mov eax, 0
   jmp return
 
+; 1 zurückgeben
 n_is_one:
   mov eax, 1
   jmp return
 
+; Funktion verlassen
 return:
   pop ebx
   leave
