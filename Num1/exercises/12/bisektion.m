ec1280b0d1b5059946fb2cc2019713ea8eeaec4c
Marten Lienen <marten.lienen@gmail.com>
2014-10-17 21:27:15 +0200

    Last exercises from last semester

--- Num1/exercises/12/bisektion.m
+++ Num1/exercises/12/bisektion.m
@@ -1,3 +1,13 @@
-function x = bisektion (f, a, b)
+function x = bisektion (f, a, b, epsilon)
+  if abs(a - b) < epsilon
+    x = a;
+  else
+    m = (a + b) / 2;
 
+    if f(a) * f(m) < 0
+      x = bisektion(f, a, m, epsilon);
+    else
+      x = bisektion(f, m, b, epsilon);
+    end
+  end
 end
